aa523260d251f17547f859980f05310831e788ab
Denny_Kate <dennykate22@gmail.com>
2023-08-02 12:06:56 +0630

    changes in models

--- models/auth.ts
+++ models/auth.ts
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
 const AuthSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  password_confirmation: String,
-  profile: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  password_confirmation: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    type: String,
+    required: true,
+  },
 });
 
 const Auth = mongoose.model("auth", AuthSchema);
--- models/blog.ts
+++ models/blog.ts
@@ -1,12 +1,30 @@
 import mongoose from "mongoose";
 
 const BlogSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  thumbnail: String,
-  slug: String,
-  upload_by: Object,
-  created_at: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  upload_by: {
+    type: Object,
+    required: true,
+  },
+  created_at: {
+    type: String,
+    required: true,
+  },
   likes: {
     type: Array,
     default: [],
